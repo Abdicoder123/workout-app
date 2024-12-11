6e74b38d110f00f01b3f4792e219acf4484eb14e
Abdirahman <abdirahman@Abdirahmans-MacBook-Air-2.local>
2024-12-11 12:01:05 -0600

    Ran rubocop to fix any offenses and now all offences are fixed

--- app/controllers/bmis_controller.rb
+++ app/controllers/bmis_controller.rb
@@ -1,60 +1,59 @@
 class BmisController < ApplicationController
     before_action :set_bmi, only: %i[show edit update destroy]
     before_action :authenticate_user!
-  
+
 
     def index
-      @bmis = Bmi.where(user_id: current_user.id) 
+      @bmis = Bmi.where(user_id: current_user.id)
     end
-  
+
     def show
     end
-  
+
     def new
       @bmi = Bmi.new
     end
-  
+
     def edit
     end
 
     def create
       @bmi = Bmi.new(bmi_params)
-      @bmi.user = current_user 
-  
+      @bmi.user = current_user
+
       if @bmi.save
-        redirect_to @bmi, notice: 'BMI was successfully created.'
+        redirect_to @bmi, notice: "BMI was successfully created."
       else
         render :new
       end
     end
-  
+
     # PATCH/PUT /bmis/1
     def update
       if @bmi.update(bmi_params)
-        redirect_to @bmi, notice: 'BMI was successfully updated.'
+        redirect_to @bmi, notice: "BMI was successfully updated."
       else
         render :edit
       end
     end
-  
+
     # DELETE /bmis/1
     def destroy
       @bmi.destroy
-      redirect_to bmis_url, notice: 'BMI was successfully destroyed.'
+      redirect_to bmis_url, notice: "BMI was successfully destroyed."
     end
-  
+
     private
-  
+
     def set_bmi
       @bmi = Bmi.find(params[:id])
-      
+
       unless @bmi.user == current_user
-        redirect_to bmis_path, alert: 'You are not authorized to view this BMI.'
+        redirect_to bmis_path, alert: "You are not authorized to view this BMI."
       end
     end
-  
+
     def bmi_params
       params.require(:bmi).permit(:height_ft, :height_in, :weight, :number, :category)
     end
-  end
-  
+end
--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,24 +1,24 @@
 class WorkoutsController < ApplicationController
     before_action :authenticate_user!
-  
+
     def index
-      @workouts = Workout.where(user_id: current_user.id) 
+      @workouts = Workout.where(user_id: current_user.id)
     end
-  
+
     def new
-      @workout = Workout.new  
+      @workout = Workout.new
     end
-  
+
     def create
       @workout = Workout.new(workout_params.merge(user_id: current_user.id))
-    
+
       if @workout.save
         respond_to do |format|
           format.html do
-            redirect_to @workout, notice: 'Workout was successfully created.'
+            redirect_to @workout, notice: "Workout was successfully created."
           end
           format.json do
-            render json: { message: 'Workout created successfully!', workout: @workout }, status: :created
+            render json: { message: "Workout created successfully!", workout: @workout }, status: :created
           end
         end
       else
@@ -33,37 +33,36 @@ class WorkoutsController < ApplicationController
         end
       end
     end
-    
-  
+
+
     def show
-      @workout = Workout.find(params[:id])  
+      @workout = Workout.find(params[:id])
     end
-  
+
     def edit
-      @workout = Workout.find(params[:id])  
+      @workout = Workout.find(params[:id])
     end
-  
+
     def update
-      @workout = Workout.find(params[:id]) 
-  
-      if @workout.update(workout_params) 
-        redirect_to @workout, notice: 'Workout was successfully updated.'  
+      @workout = Workout.find(params[:id])
+
+      if @workout.update(workout_params)
+        redirect_to @workout, notice: "Workout was successfully updated."
       else
-        render :edit 
+        render :edit
       end
     end
-  
+
     def destroy
-      @workout = Workout.find(params[:id])  
-      @workout.destroy  
-  
-      redirect_to workouts_url, notice: 'Workout was successfully destroyed.'  
+      @workout = Workout.find(params[:id])
+      @workout.destroy
+
+      redirect_to workouts_url, notice: "Workout was successfully destroyed."
     end
-  
+
     private
-  
+
     def workout_params
       params.require(:workout).permit(:name, :description, :muscle_group, :equipment, :category)
     end
-  end
-  
+end
